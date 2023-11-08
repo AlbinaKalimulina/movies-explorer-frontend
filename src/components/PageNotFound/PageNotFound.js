c56b8ab3b3b3b6fbf8e830f763eed433839df83a
Albina Kalimulina <albinashamig@yandex.ru>
2023-11-08 11:19:29 +0500

    исправлена навигация между страницами и отредактированы ссылки на внешние ресурсы

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function PageNotFound() {
+  const navigate = useNavigate()
   return (
     <section className="not-found">
       <h2 className="not-found__title">404</h2>
       <p className="not-found__subtitle">Страница не найдена</p>
-      <Link to="/" className="not-found__link">
+      <Link to="#" onClick={() => navigate(-1)} className="not-found__link">
         Назад
       </Link>
     </section>
