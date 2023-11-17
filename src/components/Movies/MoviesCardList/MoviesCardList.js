de7ad61c949c6f0d1bdb9c611b636eb4b3f15818
Albina Kalimulina <albinashamig@yandex.ru>
2023-11-17 18:05:06 +0500

    диплом

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -4,36 +4,43 @@ import { useEffect, useMemo, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 function MoviesCardList({ movies, savedMovies, onSaveMovie, onDeleteMovie, isSavedMoviesPage }) {
-  let size = useScreenResize();
-  const [moviesToAdd, setMoviesToAdd] = useState(0);
+
+  let screenSize = useScreenResize();
+
+  const [additionalMoviesToAdd, setAdditionalMoviesToAdd] = useState(0);
   const location = useLocation();
 
+  // сброс количества добавленных фильмов при изменении списка фильмов
   useEffect(() => {
-    setMoviesToAdd(0);
+    setAdditionalMoviesToAdd(0);
   }, [movies]);
 
-  const moviesToRender = useMemo(() => {
+  // расчет списка фильмов для отображения с учетом размера экрана и текущей страницы
+  const displayedMovies = useMemo(() => {
     if (isSavedMoviesPage) {
       return movies;
     }
-    let countToRender = 12;
 
-    if (size.width < 1240) {
-      countToRender = 8;
+    // определяет количество фильмов для отображения в зависимости от размера экрана
+    let countToDisplay = 12;
+
+    if (screenSize.width < 1240) {
+      countToDisplay = 8;
     }
 
-    if (size.width < 768) {
-      countToRender = 5;
+    if (screenSize.width < 768) {
+      countToDisplay = 5;
     }
 
-    return movies.slice(0, countToRender + moviesToAdd);
-  }, [movies, moviesToAdd, size, isSavedMoviesPage]);
+    return movies.slice(0, countToDisplay + additionalMoviesToAdd);
+  }, [movies, additionalMoviesToAdd, screenSize, isSavedMoviesPage]);
 
 
   return (
     <section className="card-list">
       <ul className="card-list__container">
-        {moviesToRender.map((movie) => {
+
+        {displayedMovies.map((movie) => {
           return (
             <MoviesCard
               key={movie.id || movie.movieId}
@@ -45,11 +52,14 @@ function MoviesCardList({ movies, savedMovies, onSaveMovie, onDeleteMovie, isSav
           );
         })}
       </ul>
+
       {location.pathname === '/movies' &&
-        movies.length > moviesToRender.length && (
+        movies.length > displayedMovies.length && (
           <button
             onClick={() => {
-              setMoviesToAdd((prev) => prev + (size.width >= 1241 ? 3 : 2));
+
+              // увеличение количества добавляемых фильмов в зависимости от размера экрана
+              setAdditionalMoviesToAdd((prev) => prev + (screenSize.width >= 1241 ? 3 : 2));
             }}
             className="card-list__button"
           >
--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,31 +1,36 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from '../../../images/search.svg';
 
 const SearchForm = ({ onFilter, searchQuery, filteredMovies }) => {
-  const isChecked = JSON.parse(localStorage.getItem('filterCheckBox'));
-  const [isShortFilmChecked, setIsShortFilmChecked] = useState(isChecked);
-  const [searchText, setSearchText] = useState('');
-  const [error, setError] = useState('');
+  const isChecked = JSON.parse(localStorage.getItem('filterCheckBox')); // получение состояния флажка короткометражек из локального хранилища
+  const [isShortFilmChecked, setIsShortFilmChecked] = useState(isChecked);   // состояние флажка короткометражек
+  const [searchText, setSearchText] = useState(''); // текст поиска
+  const [error, setError] = useState('');   // отображение ошибок
 
+  // обновление текста поиска
   useEffect(() => {
     if (searchQuery.searchText) {
       setSearchText(searchQuery.searchText);
     }
   }, [searchQuery.searchText]);
 
+  // обновление состояния флажка короткометражек
   useEffect(() => {
     setIsShortFilmChecked(searchQuery.isShortFilmChecked || false);
   }, [searchQuery.isShortFilmChecked]);
 
+  // переключение состояния флажка короткометражек и применение фильтрации
   const checkFilterBox = () => {
     const newIsShortFilmChecked = !isShortFilmChecked;
     setIsShortFilmChecked(newIsShortFilmChecked);
+
+    // сохранение состояния флажка в локальное хранилище
     localStorage.setItem('filterCheckBox', JSON.stringify(newIsShortFilmChecked));
 
+    // Применение фильтрации, если есть отфильтрованные фильмы
     if (filteredMovies.length > 0) {
       onFilter({ searchText, isShortFilmChecked: newIsShortFilmChecked });
     }
-
   };
 
   const onSubmit = (e) => {
@@ -40,12 +45,12 @@ const SearchForm = ({ onFilter, searchQuery, filteredMovies }) => {
 
   return (
     <section className="searchform">
+
       <form className="searchform__form-wrapper" action="#" name="search-form" onSubmit={onSubmit} noValidate>
         <div className="searchform__form">
           <div className="searchform__input-container">
             <div className="searchform__icon"></div>
             <div className="searchform__input-wrapper">
-
               <input
                 type="text"
                 className="searchform__input"
@@ -57,19 +62,15 @@ const SearchForm = ({ onFilter, searchQuery, filteredMovies }) => {
                 value={searchText}
                 onChange={(e) => setSearchText(e.target.value.toLowerCase())}
               />
-
             </div>
           </div>
-
 
           <button className="searchform__btn" type="submit" src={Search}>
             Найти
           </button>
-
         </div>
 
         <div className="searchform__filter-container">
-
           <p className="searchform__filter-title" htmlFor="short-films">Короткометражки</p>
 
           <div className="searchform__filter">
@@ -81,14 +82,11 @@ const SearchForm = ({ onFilter, searchQuery, filteredMovies }) => {
               checked={isShortFilmChecked}
             />
           </div>
-
         </div>
-
-      </form >
+      </form>
 
       {error && <p className="search-form__error">{error}</p>}
-
-    </section >
+    </section>
   );
 };
 
